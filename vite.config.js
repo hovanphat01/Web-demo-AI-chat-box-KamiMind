c06277bdffb5b423d55d1567db2c39cb7e38e64a
hovanphat01 <59350792+hovanphat01@users.noreply.github.com>
2026-07-17 22:49:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
   base: '/ai-lab/',
 })
